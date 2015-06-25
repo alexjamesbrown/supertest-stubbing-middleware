4ef517096702f3f62e034f4fcb55eeb025d924f3
Alex Brown <alex@alexjamesbrown.com>
2015-06-25 19:04:49 +0100

    refactored tests to stub out authentication middleware and test it was called after each test

--- authentication.js
+++ authentication.js
@@ -1,5 +1,7 @@
 module.exports.ensureAuthenticated = function(req, res, next) {
 
+    console.log('I am authenticating...')
+
     var authToken = req.get('x-auth-token');
 
     if (!authToken)
--- test/test.js
+++ test/test.js
@@ -1,65 +1,56 @@
-var agent;
-
-var assert = require('assert');
+var agent,
+    assert = require('assert'),
+    authentication = require('../authentication'),
+    sinon = require('sinon');
 
 describe('My routes', function() {
 
+    var ensureAuthenticatedSpy;
+
     before(function() {
+
+        //important to stub before we load our app
+        ensureAuthenticatedSpy = sinon.stub(authentication, 'ensureAuthenticated');
+
+        //this ensures we call our next() function on our middleware
+        ensureAuthenticatedSpy.callsArg(2);
+
         agent = require('supertest')
             .agent(require('../app'));
     });
 
+    afterEach(function() {
+        //assert that our middleware was called once for each test
+        sinon.assert.calledOnce(ensureAuthenticatedSpy);
+
+        ensureAuthenticatedSpy.reset();
+    })
+
     describe('GET /', function() {
-        describe('with authentication header set', function() {
-            it('returns hello', function(done) {
-                agent
-                    .get('/')
-                    .set('X-Auth-Token', 'xyz123')
-                    .expect(200)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        assert(res.text == 'Hello')
-                        done();
-                    });
-            });
-        });
-        describe('without authentication header set', function() {
-            it('returns a 401', function(done) {
-                agent
-                    .get('/')
-                    .expect(401)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        done();
-                    });
-            });
+        it('returns hello', function(done) {
+            agent
+                .get('/')
+                .set('X-Auth-Token', 'xyz123')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    assert(res.text == 'Hello')
+                    done();
+                });
         });
     });
 
     describe('POST /', function() {
-        describe('with authentication header set', function() {
-            it('returns hello', function(done) {
-                agent
-                    .post('/')
-                    .set('X-Auth-Token', 'xyz123')
-                    .expect(200)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        assert(res.text == 'Posted')
-                        done();
-                    });
-            });
-        });
-        describe('without authentication header set', function() {
-            it('returns a 401', function(done) {
-                agent
-                    .post('/')
-                    .expect(401)
-                    .end(function(err, res) {
-                        if (err) return done(err);
-                        done();
-                    });
-            });
+        it('returns hello', function(done) {
+            agent
+                .post('/')
+                .set('X-Auth-Token', 'xyz123')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    assert(res.text == 'Posted')
+                    done();
+                });
         });
     });
 });
